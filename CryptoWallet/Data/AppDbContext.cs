06905c6f1db43b9f6df1dfd9497b2511434b80c0
NeKsiKZ <bz12330@gmail.com>
2026-04-07 14:00:13 +0200

    feat: add AppDbContext and configure connection string

---
+++ CryptoWallet/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CryptoWallet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoWallet.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<CryptoAsset> CryptoAssets { get; set; }
+    }
+}
