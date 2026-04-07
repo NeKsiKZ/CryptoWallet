6dd427ca2fc892c9e81af6bf51d6b45e0231771d
NeKsiKZ <bz12330@gmail.com>
2026-04-07 13:55:43 +0200

    feat: add CryptoAsset model

---
+++ CryptoWallet/Models/CryptoAsset.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoWallet.Models
+{
+    public class CryptoAsset
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public decimal Quantity { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public DateTime PurchaseDate { get; set; } = DateTime.UtcNow;
+    }
+}
